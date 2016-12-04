9582a8a0816187e6512950abf53e7a8c598b0898
root <root@lone.spark.TwitterArchive.one>
2016-12-04 15:41:40 -0600

    Added gnip sentiment label

--- analyze_tweets/getMessages.py
+++ analyze_tweets/getMessages.py
@@ -13,9 +13,17 @@ sys.path.insert(0,os.path.join(spark_home,'python/lib/py4j-0.9-src.zip'))
 execfile(os.path.join(spark_home,'python/pyspark/shell.py'))
 
 
-f = sc.textFile("/data/archive/file_20160417_0000/part-00000")
-d = f.map(lambda x: json.loads(x))
-blah = d.map(lambda x: x["tweets"]).map(lambda y: y)
-all = sc.parallelize(blah.take(1)[0])
-messages = all.map(lambda x: x["message"]["body"])
-pprint(messages.take(10))
+f = sc.textFile("/data/archive/*/part-0000*")
+#jsonGnip = f.map(lambda x: json.loads(x))
+#jsonTweetsEmbedded = jsonGnip.map(lambda x: x["tweets"])
+#jsonTweets = sc.parallelize(jsonTweetsEmbedded.take(10000))
+#twts = jsonTweetsEmbedded.map(lambda x: x[0]["message"]["body"])
+#messages = twts.map(lambda x: x[0]["message"]["body"])
+#pprint(messages.take(10000))
+#print len(messages.take(10000))
+
+#More compact
+messages = f.map(lambda x: json.loads(x)).map(lambda x: x["tweets"]).map(lambda x: [x[0]["message"]["body"],x[0]["cde"]["content"]["sentiment"]["polarity"]])
+proof = messages.take(int(sys.argv[1]))
+pprint(proof)
+pprint(len(proof))
