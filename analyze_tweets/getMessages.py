1b4e692da87196d0bb416cf9411a133b06962fdb
root <root@lone.spark.TwitterArchive.one>
2016-12-04 16:31:42 -0600

    Added naive sentiment analysis using Andy's word rankings

--- analyze_tweets/getMessages.py
+++ analyze_tweets/getMessages.py
@@ -12,6 +12,12 @@ sys.path.insert(0,os.path.join(spark_home,'python'))
 sys.path.insert(0,os.path.join(spark_home,'python/lib/py4j-0.9-src.zip'))
 execfile(os.path.join(spark_home,'python/pyspark/shell.py'))
 
+fW = open('sentimentRankedWordList.txt','r')
+all=fW.readlines()
+head = all[0].strip().split('\t')
+words = [line.strip().split('\t') for line in all]
+wordMap = {line[0]:{head[1]:line[1], head[2]:line[2], head[3]:line[3], head[4]:line[4]} for line in words[1:]}
+wmBroadcast = sc.broadcast(wordMap)
 
 f = sc.textFile("/data/archive/*/part-0000*")
 #jsonGnip = f.map(lambda x: json.loads(x))
@@ -23,7 +29,11 @@ f = sc.textFile("/data/archive/*/part-0000*")
 #print len(messages.take(10000))
 
 #More compact
-messages = f.map(lambda x: json.loads(x)).map(lambda x: x["tweets"]).map(lambda x: [x[0]["message"]["body"],x[0]["cde"]["content"]["sentiment"]["polarity"]])
+messages = f.map(lambda x: json.loads(x))\
+	    .map(lambda x: x["tweets"])\
+	    .map(lambda x: [x[0]["message"]["body"],x[0]["cde"]["content"]["sentiment"]["polarity"]])
+sentiment = messages.map(lambda x: [x[0], x[1], sum([float(wmBroadcast.value.get(word).get("happiness_average")) for word in x[0].split() if word in wmBroadcast.value])])
 proof = messages.take(int(sys.argv[1]))
 pprint(proof)
 pprint(len(proof))
+pprint(sentiment.take(int(sys.argv[1])))
