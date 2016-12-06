6ac986b6eb67d896836f121594e28904c6ec132b
root <root@lone.spark.TwitterArchive.one>
2016-12-05 21:45:45 -0600

    Tacking on additional fields from the json

--- analyze_tweets/getMessages.py
+++ analyze_tweets/getMessages.py
@@ -20,20 +20,15 @@ wordMap = {line[0]:{head[1]:line[1], head[2]:line[2], head[3]:line[3], head[4]:l
 wmBroadcast = sc.broadcast(wordMap)
 
 f = sc.textFile("/data/archive/*/part-0000*")
-#jsonGnip = f.map(lambda x: json.loads(x))
-#jsonTweetsEmbedded = jsonGnip.map(lambda x: x["tweets"])
-#jsonTweets = sc.parallelize(jsonTweetsEmbedded.take(10000))
-#twts = jsonTweetsEmbedded.map(lambda x: x[0]["message"]["body"])
-#messages = twts.map(lambda x: x[0]["message"]["body"])
-#pprint(messages.take(10000))
-#print len(messages.take(10000))
 
 #More compact
 messages = f.map(lambda x: json.loads(x))\
 	    .map(lambda x: x["tweets"])\
-	    .flatMap(lambda x: [[i["message"]["body"],i["cde"]["content"]["sentiment"]["polarity"]] for i in x])
-sentiment = messages.map(lambda x: [x[0], x[1], sum([float(wmBroadcast.value.get(word).get("happiness_average")) for word in x[0].split() if word in wmBroadcast.value])])
+	    .flatMap(lambda x: [[i["message"]["body"],\
+            	i["cde"]["author"]["location"],\
+            	i["message"]["postedTime"],\
+            	i["cde"]["content"]["sentiment"]["polarity"],\
+            	sum([float(wmBroadcast.value.get(word).get("happiness_average")) for word in i["message"]["body"].split() if word in wmBroadcast.value])] for i in x])
 proof = messages.take(int(sys.argv[1]))
 pprint(proof)
 pprint(len(proof))
-pprint(sentiment.take(int(sys.argv[1])))
