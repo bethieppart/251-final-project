3cca81c27dc60f6d293bd27437edb0a718702aa2
Anthony Spalvieri-kruse <ask417@nyu.edu>
2016-12-11 18:34:07 -0800

    sss

--- analyze_tweets/TwitterAnaysis.R
+++ analyze_tweets/TwitterAnaysis.R
@@ -1,6 +1,6 @@
 require("ggplot2")
 
-df = read.csv("C:/Users/anthony.spalvieri/Downloads/CombinedOutput.csv", header = FALSE)
+df = read.csv("../../CombinedOutput.csv", header = FALSE)
 colnames(df)<-c("date", "location", "label", "tweetCount", "avgPolarity", "totalSentiment","avgSentiment","timestamp")
 df$location <- sapply(df$location, tolower)
 names(df$location)<-NULL
@@ -13,7 +13,7 @@ A<-df[df$location %in% c("united states", stateNames),]
 by(A$location, A$location, length)
 by(A$location, A$label, length)
 
-
+pdf("TwitterElection.pdf")
 ggplot(A, aes(x=label, y=tweetCount, fill=label)) + stat_summary(fun.y="sum", geom="bar") +theme(axis.text.x = element_text(angle = 90)) + ggtitle("Tweet Counts By Topic") + ylab("Tweet Count")
 ggplot(A, aes(x=label, y=avgPolarity, fill=label)) + stat_summary(fun.y="mean", geom="bar") +theme(axis.text.x = element_text(angle = 90)) + ggtitle("Average Polarity By Topic") + ylab("Tweet Count")
 ggplot(A, aes(x=label, y=avgSentiment, fill=label)) + stat_summary(fun.y="mean", geom="bar") +theme(axis.text.x = element_text(angle = 90)) + ggtitle("Average Sentiment By Topic") + ylab("Tweet Count")
@@ -32,4 +32,4 @@ ggplot(A[A$location=="united states",],aes(x=date, y=avgSentiment, group=label,c
 #Need to get 538 polling data, then splice it into my time series charts.  
 #Maps would be nice, but seems like something more for tableau or another teammate to do
 
-
+dev.off()
